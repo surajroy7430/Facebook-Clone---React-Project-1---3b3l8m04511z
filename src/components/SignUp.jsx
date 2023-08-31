dc2b754f8a70b37c8e59f33c1cf87385d2b46728
Suraj Roy <surajroy7430@gmail.com>
2023-08-31 19:15:49 +0530

    .

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,4 +1,3 @@
-import { WidthFull } from '@mui/icons-material';
 import { 
     Button,
     Container, 
@@ -7,31 +6,15 @@ import {
     FormControlLabel, 
     FormLabel, 
     Grid, 
-    // MenuItem, 
     Radio, 
-    RadioGroup, 
-    // Select, 
+    RadioGroup,  
     TextField, 
     Typography 
 } from '@mui/material';
-// import makeStyles from '@mui/material';
-// import React, { useState } from 'react';
 
 
 const SignUp = () => {
-    // const [selectedDay, setSelectedDay] = useState('');
-    // const [selectedMonth, setSelectedMonth] = useState('');
-    // const [selectedYear, setSelectedYear] = useState('');
-
-    // const handleDayChange = (e) => {
-    //     setSelectedDay(e.target.value);
-    // }
-    // const handleMonthChange = (e) => {
-    //     setSelectedMonth(e.target.value);
-    // }
-    // const handleYearChange = (e) => {
-    //     setSelectedYear(e.target.value);
-    // }
+    
   return (
     <div>
         <CssBaseline />
@@ -104,58 +87,5 @@ const SignUp = () => {
     </div>
   )
 }
-
-// const DobFormControl = ({
-//     selectedDay, 
-//     selectedMonth, 
-//     selectedYear, 
-//     handleDayChange, 
-//     handleMonthChange, 
-//     handleYearChange, 
-// }) => {
-//     const daysInMonth = (year, month) => {
-//         return new Date(year, month, 0).getDate();
-//     }
-//     const generateDays = () => {
-//         if(selectedMonth && selectedYear){
-//             const numDays = daysInMonth(selectedMonth, selectedYear);
-//             return Array.from({length: numDays}, (_, index) => index+1);
-//         }
-//         return [];
-//     }
-//   return (
-//     <FormControl fullWidth>
-//         <FormLabel>Date Of Birth</FormLabel>
-//         <Grid container spacing={2}>
-//             <Grid item xs={4}>
-//                 <Select value={selectedDay} onChange={handleDayChange}>
-//                     <MenuItem>Day</MenuItem>
-//                     {generateDays.map((day) => (
-//                         <MenuItem key={day} value={day}>{day}</MenuItem>
-//                     ))}
-//                 </Select>
-//             </Grid>
-//             <Grid item xs={4}>
-//                 <Select value={selectedMonth} onChange={handleMonthChange}>
-//                     <MenuItem>Month</MenuItem>
-//                     {Array.form({length: 12}, (_, index) => (
-//                         <MenuItem key={index} value={index+1}>{index+1}</MenuItem>
-//                     ))}
-//                 </Select>
-//             </Grid>
-//             <Grid item xs={4}>
-//                 <Select value={selectedYear} onChange={handleYearChange}>
-//                     <MenuItem>Year</MenuItem>
-//                     {Array.from({length: new Date().getFullYear() - 1899}, (_, index) => (
-//                         <MenuItem key={index} value={new Date().getFullYear() - index}>
-//                             {new Date().getFullYear() - index}
-//                         </MenuItem>
-//                     ))}
-//                 </Select>
-//             </Grid>
-//         </Grid>
-//     </FormControl>
-//   )
-// }
 
 export default SignUp
